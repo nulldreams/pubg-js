a91bba1c1eea6966c825bcc5f4023e84e6c109bc
Igor Souza Martins <nulldreams@gmail.com>
2018-04-27 16:38:25 -0300

    feat: arquivos separados, novas funções para pesquisas

--- README.md
+++ README.md
@@ -1,3 +1,55 @@
 # pubg-js
+A PubgAPI SDK for javascript
 
-in development
+#### `pubg.setToken(token)`
+```javascript
+console.log(await pubg.setToken(token))
+// {"data":{"type":"status","id":"pubg-api","attributes":{"version":"v9.2.0","releasedAt":"2018-04-25T20:40:13Z"}}}
+```
+#### `pubg.player.info(nickname)`
+```javascript
+let player = JSON.parse(await pubg.player.info('pc-na', 'igor-martins'))
+console.log(`Player ID: ${player.data[0].id}`)
+
+// Player ID: account.67ad0292daf94a90960357bfd17764e8
+```
+#### `pubg.player.infoById(nickname)`
+```javascript
+let player = JSON.parse(await pubg.player.infoById('pc-na', 'account.67ad0292daf94a90960357bfd17764e8'))
+console.log(`Nickname: ${player.data[0].attributes.name}`)
+
+// Nickname: igor-martins
+```
+#### `pubg.match.winner.player(region, matchID)`
+```javascript
+let winner = await pubg.match.winner.player('pc-na', 'b9281965-4a12-487b-a544-a3c3b58faba3')
+console.log(winner)
+```
+```json
+{ data:
+   [ { type: 'participant',
+       id: '3fe34769-7023-4019-a5c7-b55ef26ec96c',
+       attributes: [Object] },
+     { type: 'participant',
+       id: '010d0fbe-6e13-435a-94a9-59db6245af3f',
+       attributes: [Object] },
+     { type: 'participant',
+       id: '9eb4435f-7c99-4dea-8845-89415b8cf1f5',
+       attributes: [Object] } ],
+  names: [ 'Uzuzy', 'The_Fake998', 'Phi_Senpai' ] }
+```
+#### `pubg.match.winner.team(region, matchID)`
+```javascript
+let winner = await pubg.match.winner.team('pc-na', 'b9281965-4a12-487b-a544-a3c3b58faba3')
+console.log(winner)
+```
+```json
+[ { type: 'roster',
+    id: 'a990d773-a145-43a6-ad37-34aa8a8dd267',
+    attributes: { stats: [Object], won: 'true', shardId: 'pc-na' },
+    relationships: { team: [Object], participants: [Object] } } ]
+```
+
+#### in development...
+
+
--- index.js
+++ index.js
@@ -2,21 +2,35 @@ const pubg = require('./lib')
 let token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIxMTI1MDg1MC0yYjhmLTAxMzYtNWQwOS02NWE1M2MxZDY1YTMiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTI0NzU0MjIxLCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6InB1YmctYm90LWFwaSIsInNjb3BlIjoiY29tbXVuaXR5IiwibGltaXQiOjEwfQ.WJxfmZDgJoM6lO6QmJ5fHN6iT8qlY8JdYqb6kn2u_js'
 
 async function test () {
-  pubg.setToken(token)
+  await pubg.setToken(token)
 
-  // let player = JSON.parse(await pubg.player.infoById('pc-na', 'account.67ad0292daf94a90960357bfd17764e8'))
+  // let player = JSON.parse(await pubg.player.info('pc-na', 'igor-martins'))
 
-  // console.log(`Nickname: ${player.data[0].attributes.name}`)
+  // console.log(`Player ID: ${player.data[0].id}`)
   // console.log(await pubg.telemetry.url('pc-na', 'b9281965-4a12-487b-a544-a3c3b58faba3'))
   // let resultado = await pubg.telemetry.url('pc-na', 'b9281965-4a12-487b-a544-a3c3b58faba3')
   // let players = await pubg.players.info('pc-na', 'igor-martins')
-  let search = {
-    node: 'included',
-    query: { name: 'igor-martins' }
-  }
-  
-  let resultado = await pubg.match.players.find('pc-na', 'b9281965-4a12-487b-a544-a3c3b58faba3', search)
-  console.log(resultado)
+  // let search = {
+  //   node: 'included',
+  //   query: { 'name': 'igor-martins' }
+  // }
+  // let search = {
+  //   node: 'included',
+  //   query: [ 'attributes.stats.killPointsDelta', 10.2518635 ]
+  // }
+  // let search = {
+  //   node: 'included',
+  //   query: function (v) {
+  //     if (v.type === 'participant') { return v.attributes.stats.winPlace < 2 }
+  //   }
+  // }
+  // let players = []
+  let winner = await pubg.match.winner.team('pc-na', 'b9281965-4a12-487b-a544-a3c3b58faba3')
+  console.log(winner)
+  // resultado.forEach(element => {
+  //   players.push()
+  // })
+  // console.log(resultado)
 }
 
 test()
